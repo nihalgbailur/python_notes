8204bf855fe9cdc55ca5431641927babdd04e782
NIHAL G BAILUR <nihalgbailur@NIHALs-MacBook-Air.local>
2024-02-05 20:44:25 +0530

    index

---
+++ index.py
@@ -0,0 +1,11 @@
+# String
+my_string = "Python"
+
+# Accessing individual characters
+first_char = my_string[0]  # 'P'
+third_char = my_string[2]  # 't'
+
+# Negative indexing (counting from the end)
+last_char = my_string[-1]  # 'n'
+second_last_char = my_string[-2]  # 'o'
+print(first_char)
--- strings.py
+++ strings.py
@@ -1,3 +1,13 @@
 s1="nihal"
 s2="win"
-print(s1+s2)
+final =(s1+s2)
+print(len(final))
+
+
+s1="nihal"
+s2="win"
+final_2= (s1+" "+s2) 
+# space is also considered as count
+print(len(final_2))
+
+
